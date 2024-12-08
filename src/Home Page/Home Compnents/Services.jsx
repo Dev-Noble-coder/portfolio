3dc5d7b2eef62e4e84e7f9db21d2603f5aee5d31
Adebayo Favour <fadebayo637@gmail.com>
2024-12-08 13:09:16 +0100

    Changes

--- src/Home Page/Home Compnents/Services.jsx
+++ src/Home Page/Home Compnents/Services.jsx
@@ -21,32 +21,32 @@ const Services = () => {
                 <p className='font-semibold'>What Can I Do</p>
             </div>
             <div className='grid grid-cols-1 sm:grid-cols-2 w-full lg:grid-cols-3 px-4 sm:px-20 lg:px-36 gap-5'>
-                <div className='bg-zinc-950 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-up">
+                <div className='bg-zinc-800 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-up">
                     <span><i class='bx bxs-pencil text-[#FF014F] text-4xl'></i></span>
                     <p className='font-semibold'>Creative Design</p>
               
                 </div>
-                <div className='bg-zinc-950 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-down">
+                <div className='bg-zinc-800 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-down">
                     <span><i class='bx bxs-flag-alt  text-[#FF014F] text-4xl'></i></span>
                     <p className='font-semibold'>Branding</p>
                
                 </div>
-                <div className='bg-zinc-950 py-10 px-12 flex justify-center flex-col items-center'data-aos="fade-up">
+                <div className='bg-zinc-800 py-10 px-12 flex justify-center flex-col items-center'data-aos="fade-up">
                     <span><i class='bx bx-laptop  text-[#FF014F] text-4xl'></i></span>
                     <p className='font-semibold'>User Interface</p>
                   
                 </div>
-                <div className='bg-zinc-950 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-up">
+                <div className='bg-zinc-800 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-up">
                     <span><i class='bx bxs-briefcase  text-[#FF014F] text-4xl'></i></span>
                     <p className='font-semibold'>User Experience</p>
                 
                 </div>
-                <div className='bg-zinc-950 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-down">
+                <div className='bg-zinc-800 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-down">
                     <span><i class='bx bx-code-alt  text-[#FF014F] text-4xl'></i></span>
                     <p className='font-semibold'>Clean Code</p>
                  
                 </div>
-                <div className='bg-zinc-950 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-up">
+                <div className='bg-zinc-800 py-10 px-12 flex justify-center flex-col items-center' data-aos="fade-up">
                     <span><i class='bx bx-support  text-[#FF014F] text-4xl'></i></span>
                     <p className='font-semibold'>Fast Support</p>
                  
